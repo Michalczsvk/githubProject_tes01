165a93b802a9ff06a30927e746959c8035ae4d95
Michal <m.lukac@icloud.com>
2022-12-26 16:23:09 +0100

    Initial Commit

---
+++ githubProject/githubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubProjectApp.swift
+//  githubProject
+//
+//  Created by Michal Lukac on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct githubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
